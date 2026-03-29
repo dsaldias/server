28ae79bf22319da71306d9ae88882a166b211210
diego saldias <diego.saldias@esam.edu.bo>
2026-03-29 12:58:37 -0400

    ft. renombre de tablas pos

--- generar/sql/database.sql
+++ generar/sql/database.sql
@@ -1,4 +1,4 @@
-create table `usuarios`(
+create table `rbac_usuarios`(
     `id` integer unsigned auto_increment not null primary key,
     `nombres` varchar(60) not null,
     `apellido1` varchar(30) not null,
@@ -19,7 +19,7 @@ create table `usuarios`(
     `fecha_update` datetime not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-create table `roles`(
+create table `rbac_roles`(
     `id` smallint unsigned auto_increment not null primary key,
     `nombre` varchar(50) not null unique,
     `descripcion` varchar(100),
@@ -28,7 +28,7 @@ create table `roles`(
 );
 
 -- sedes, departamentos, ubicaciones segun nomenclatura
-create table `unidades`(
+create table `rbac_unidades`(
     `id` smallint unsigned auto_increment not null primary key,
     `nombre` varchar(70) not null unique,
     `descripcion` varchar(100),
@@ -39,7 +39,7 @@ create table `unidades`(
 
 -- para permisos no hay crud, se agregan segun se crean las funcionaes 
 -- en el propio codigo fuente 
-create table `permisos`(
+create table `rbac_permisos`(
     -- en golang hay funciones(query,mutation), cada funcion representa un permiso, el nombre de esa funcion 
     -- es el valor de metodo, (ej createPersona)
     `metodo` varchar(50) not null primary key,
@@ -50,36 +50,36 @@ create table `permisos`(
     `fecha_registro` datetime not null default CURRENT_TIMESTAMP
 );
 
-create table `rol_permiso`(
+create table `rbac_rol_permiso`(
     `rol_id` smallint unsigned not null,
     `metodo` varchar(50) not null,
     `fecha_registro` datetime not null default CURRENT_TIMESTAMP,
-    foreign key(`rol_id`) references `roles`(`id`),
-    foreign key(`metodo`) references `permisos`(`metodo`),
+    foreign key(`rol_id`) references `rbac_roles`(`id`),
+    foreign key(`metodo`) references `rbac_permisos`(`metodo`),
     primary key(`rol_id`,`metodo`)
 );
 
-create table `rol_usuario_unidades`(
+create table `rbac_rol_usuario_unidades`(
     `rol_id` smallint unsigned not null,
     `usuario_id` integer unsigned not null,
     `unidad_id` smallint unsigned not null,
     `fecha_registro` datetime not null default CURRENT_TIMESTAMP,
-    foreign key(`rol_id`) references `roles`(`id`),
-    foreign key(`usuario_id`) references `usuarios`(`id`),
-    foreign key(`unidad_id`) references `unidades`(`id`),
+    foreign key(`rol_id`) references `rbac_roles`(`id`),
+    foreign key(`usuario_id`) references `rbac_usuarios`(`id`),
+    foreign key(`unidad_id`) references `rbac_unidades`(`id`),
     primary key(`rol_id`,`usuario_id`,`unidad_id`)
 );
 
-create table `usuario_permiso`(
+create table `rbac_usuario_permiso`(
     `usuario_id` integer unsigned not null,
     `metodo` varchar(50) not null,
     `fecha_registro` datetime not null default CURRENT_TIMESTAMP,
-    foreign key(`usuario_id`) references `usuarios`(`id`),
-    foreign key(`metodo`) references `permisos`(`metodo`),
+    foreign key(`usuario_id`) references `rbac_usuarios`(`id`),
+    foreign key(`metodo`) references `rbac_permisos`(`metodo`),
     primary key(`usuario_id`,`metodo`)
 );
 
-create table `menus`(
+create table `rbac_menus`(
     `id` tinyint unsigned auto_increment not null primary key,
     `label` varchar(40) not null unique,
     `path` varchar(40) not null,
@@ -88,79 +88,79 @@ create table `menus`(
     `grupo` tinyint unsigned not null default 1,
     `orden` tinyint unsigned not null default 1,
     `padre_id` tinyint unsigned null,
-    foreign key(`padre_id`) references `menus`(`id`)
+    foreign key(`padre_id`) references `rbac_menus`(`id`)
 );
 
-create table `menus_usuario`(
+create table `rbac_menus_usuario`(
     `id` smallint unsigned auto_increment not null primary key,
     `usuario_id` integer unsigned not null,
     `menu_id` tinyint unsigned not null,
     `fecha_registro` datetime not null default CURRENT_TIMESTAMP,
-    foreign key(`usuario_id`) references `usuarios`(`id`),
-    foreign key(`menu_id`) references `menus`(`id`)
+    foreign key(`usuario_id`) references `rbac_usuarios`(`id`),
+    foreign key(`menu_id`) references `rbac_menus`(`id`)
 );
 
-create table `rol_menus`(
+create table `rbac_rol_menus`(
     `id` smallint unsigned auto_increment not null primary key,
     `rol_id` smallint unsigned not null,
     `menu_id` tinyint unsigned not null,
     `fecha_registro` datetime not null default CURRENT_TIMESTAMP,
-    foreign key(`rol_id`) references `roles`(`id`),
-    foreign key(`menu_id`) references `menus`(`id`)
+    foreign key(`rol_id`) references `rbac_roles`(`id`),
+    foreign key(`menu_id`) references `rbac_menus`(`id`)
 );
 
-create table `session_keys`(
+create table `rbac_session_keys`(
     `id` integer unsigned auto_increment not null primary key,
     `usuario_id` integer unsigned not null,
     `key` varchar(80) not null unique,
     `apikey` varchar(255) not null,
     `expire` datetime not null,
     `fecha_registro` datetime not null default CURRENT_TIMESTAMP,
-    foreign key(`usuario_id`) references `usuarios`(`id`)
+    foreign key(`usuario_id`) references `rbac_usuarios`(`id`)
 );
 
-create table `notificaciones`(
+create table `rbac_notificaciones`(
     `id` integer unsigned auto_increment not null primary key,
     `mensaje` text not null, -- formato html
     `creado_por_id` integer unsigned not null,
     `desde` datetime not null,
     `hasta` datetime not null,
     `fecha_registro` datetime not null default CURRENT_TIMESTAMP,
-    foreign key(`creado_por_id`) references `usuarios`(`id`)
+    foreign key(`creado_por_id`) references `rbac_usuarios`(`id`)
 );
 
-create table `tickets`(
+create table `rbac_tickets`(
     `id` integer unsigned auto_increment not null primary key,
     `usuario_id` integer unsigned not null,
     `problema` text not null,
     `estado` enum('pendiente','cliente','soporte','cerrado') not null default 'pendiente',
     `fecha_registro` datetime not null default CURRENT_TIMESTAMP,
-    foreign key(`usuario_id`) references `usuarios`(`id`)
+    foreign key(`usuario_id`) references `rbac_usuarios`(`id`)
 );
 
-create table `tickets_respuestas`(
+create table `rbac_tickets_respuestas`(
     `id` integer unsigned auto_increment not null primary key,
     `tickets_id` integer unsigned not null,
     `usuario_id` integer unsigned not null,
     `respuesta` text not null,
     `fecha_registro` datetime not null default CURRENT_TIMESTAMP,
-    foreign key(`usuario_id`) references `usuarios`(`id`),
-    foreign key(`tickets_id`) references `tickets`(`id`)
+    foreign key(`usuario_id`) references `rbac_usuarios`(`id`),
+    foreign key(`tickets_id`) references `rbac_tickets`(`id`)
 );
 
 
 -- indice para optimizar la busqueda 
 -- CREATE INDEX idx_username ON usuarios (username);
-CREATE INDEX idx_rol_usuario_unidades ON rol_usuario_unidades(usuario_id, rol_id, unidad_id);
-CREATE INDEX idx_rol_permiso ON rol_permiso(rol_id, metodo);
-ALTER TABLE tickets AUTO_INCREMENT = 100;
+CREATE INDEX idx_rol_usuario_unidades ON rbac_rol_usuario_unidades(usuario_id, rol_id, unidad_id);
+CREATE INDEX idx_rol_permiso ON rbac_rol_permiso(rol_id, metodo);
+ALTER TABLE rbac_tickets AUTO_INCREMENT = 100;
 
-insert into `unidades`(`nombre`) values('principal');
-insert into `unidades`(`nombre`) values('secundaria');
+insert into `rbac_unidades`(`nombre`) values('principal');
+insert into `rbac_unidades`(`nombre`) values('secundaria');
 
 -- los id de menus estan reservados hasta el 10
 -- en tu app debes crearlas desde el 10 o superior
-insert into `menus`(`id`,`label`,`path`,`icon`,`grupo`,`color`,`orden`) 
+insert into `rbac_menus`(`id`,`label`,`path`,`icon`,`grupo`,`color`,`orden`) 
 values 
 (1,'Usuarios','/usuarios','group',1,'primary',1),
 (2,'Roles','/roles','local_movies',1,'primary',2),
@@ -169,7 +169,7 @@ values
 (5,'Tickets','/tickets','confirmation_number',1,'primary',5);
 
 
-INSERT INTO `permisos` (`metodo`, `nombre`, `grupo`,`descripcion`)
+INSERT INTO `rbac_permisos` (`metodo`, `nombre`, `grupo`,`descripcion`)
 VALUES
 ('roles', 'roles', 'roles','Listar roles'),
 ('permisos', 'permisos', 'permisos','Listar permisos'),
@@ -194,21 +194,30 @@ VALUES
 ('update_ticket', 'responder tickets', 'tickets','responder tickets'),
 ('cerrar_ticket', 'cerrar tickets', 'tickets','cerrar tickets');
 
-insert into `roles`(`id`,`nombre`,`descripcion`,`jerarquia`)
+insert into `rbac_roles`(`id`,`nombre`,`descripcion`,`jerarquia`)
 values 
 (1,'Administrador','Total acceso',0),
 (2,'Invitado','Bienvenido',1),
 (3,'Externo','Bienvenido',2);
 
-insert into `rol_menus`(`rol_id`,`menu_id`)
+insert into `rbac_rol_menus`(`rol_id`,`menu_id`)
 values (1,1), (1,2), (1,3), (1,4), (1,5), (2,5), (3,5);
 
-insert into `usuarios`(`nombres`,`apellido1`,`username`,`password`)
+insert into `rbac_usuarios`(`nombres`,`apellido1`,`username`,`password`)
 values ('admin','','admin',SHA2('admin', 256));
 
-insert into `rol_usuario_unidades`(`rol_id`,`usuario_id`,`unidad_id`)
+insert into `rbac_rol_usuario_unidades`(`rol_id`,`usuario_id`,`unidad_id`)
 values (1,1,1), (2,1,2);
 
-insert into `rol_permiso`(`rol_id`,`metodo`)
+insert into `rbac_rol_permiso`(`rol_id`,`metodo`)
 values (1,'roles'),(1,'rol_by_id'),(1,'update_rol'),(1,'permisos'),(1,'menus');
 
+-- 
+-- ejemplo de menus anidados: 
+-- 
+-- insert into `rbac_menus`(`id`,`label`,`path`,`icon`,`grupo`,`color`,`orden`,`padre_id`) 
+-- values 
+-- (6,'Menu anidado','/','group',2,'primary',1,null),
+-- (7,'Rolesx','/roles','local_movies',2,'primary',2, 6),
+-- (8,'Unidadesx','/unidades','home',2,'primary',3, 6),
+-- (9,'Notificacionesx','/avisos','campaign',2,'primary',4, 6);
